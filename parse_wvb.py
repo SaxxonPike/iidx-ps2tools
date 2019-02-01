b1196075a6f39b1db4852cf556fb45a31db51cf8
windyfairy <windyfairy@users.noreply.github.com>
2019-02-02 00:13:45 +0900

    Fix up newer PS2 extraction code

--- parse_wvb.py
+++ parse_wvb.py
@@ -76,7 +76,7 @@ def parse_wvb_old(infile, output_folder):
                 outfile.write(infile.read(size))
 
             wav_output_filename = output_filename.replace(".pcm", ".wav")
-            subprocess.call('test.exe -o "%s" "%s"' % (wav_output_filename, output_filename))
+            subprocess.call('vgmstream_cli.exe -o "%s" "%s"' % (wav_output_filename, output_filename))
             os.unlink(output_filename)
 
             wav = pydub.AudioSegment.from_file(wav_output_filename)
@@ -93,24 +93,22 @@ def parse_wvb_new(infile, output_folder):
 
     keysound_entries = []
     for i in range(0, (0x8000 - 0x10) // 0x10):
-        unk1, unk2, unk3, pan_l, pan_r, file_id, volume = struct.unpack("<HHIBBHI", infile.read(16))
+        unk1, unk2, duration_ms, pan, volume, file_id, _ = struct.unpack("<HHIBBHI", infile.read(16))
 
         if unk1 == 0:
             continue
 
         keysound_entries.append({
-            'unk1': unk1,
-            'unk2': unk2,
-            'unk3': unk3,
-            'pan_l': pan_l,
-            'pan_r': pan_r,
+            'duration_ms': duration_ms,
+            'pan': pan,
             'file_id': file_id,
             'volume': volume,
-            'entry_id': i,
+            'entry_id': i + 1,
+            'mix': [2, 1][(unk2 >> 8) - 1]
         })
 
-    file_count = struct.unpack("<I", infile.read(4))[0]
-    infile.seek(0x0c, 1)
+    file_count, _, is_encrypted = struct.unpack("<III", infile.read(12))
+    infile.seek(0x04, 1)
 
     file_entries = []
     for i in range(0, file_count):
@@ -120,38 +118,59 @@ def parse_wvb_new(infile, output_folder):
             'file_id': i,
             'offset': offset,
             'filesize': filesize,
-            'unk1': unk1, # Does this relate to the sample rate?
+            'unk1': unk1, # Does one of these relate to the sample rate somehow?
             'unk2': unk2,
             'unk3': unk3,
         })
 
+    data_offset = infile.tell()
+
 
     for entry in keysound_entries:
         entry_id = entry['entry_id']
-        sample_rate = 44100 #entry['sample_rate']
         size = file_entries[entry['file_id']]['filesize']
-        offset = file_entries[entry['file_id']]['offset']
+        offset = data_offset + file_entries[entry['file_id']]['offset']
         volume = entry['volume']
-        pan = 64
+        pan = entry['pan']
+        sample_rate = 44100
+        channels = 1
+
+        if file_entries[entry['file_id']]['unk2'] & 0xff == 0x44:
+            sample_rate = 44100
+
+        elif file_entries[entry['file_id']]['unk2'] & 0xff == 0x7d:
+            sample_rate = 32000
+
+        else:
+            print("Unknown sample rate", file_entries[entry['file_id']]['unk2'])
+            exit(1)
 
         output_filename = os.path.join(output_folder, "%04d.pcm" % entry_id)
 
         print("Extracting", output_filename)
 
         with open(output_filename, "wb") as outfile:
-            outfile.write(struct.pack(">IHHB", size, 0, sample_rate, 1))
-            outfile.write(bytearray([0] * 0x807))
+            outfile.write(struct.pack("<IIII", 0x08640001, 0, 0x800, size))
+            outfile.write(struct.pack("<IIII", 0, 0, sample_rate, channels))
+            outfile.write(struct.pack("<IIII", is_encrypted, 16, 0, 0))
+            outfile.write(bytearray([0] * 0x7d0))
 
             infile.seek(offset)
             outfile.write(infile.read(size))
 
         wav_output_filename = output_filename.replace(".pcm", ".wav")
-        subprocess.call('test.exe -o "%s" "%s"' % (wav_output_filename, output_filename))
+        subprocess.call('vgmstream_cli.exe -o "%s" "%s"' % (wav_output_filename, output_filename))
         os.unlink(output_filename)
 
         wav = pydub.AudioSegment.from_file(wav_output_filename)
-        wav = wav.pan((pan - (128 / 2)) / (128 / 2))
-        wav += percentage_to_db((volume / 127) * 100 * 0.75)
+
+        if entry['mix'] == 2:
+            wav = wav.set_channels(2).pan(((pan - (128 / 2)) / (128 / 2)))
+            wav += percentage_to_db((volume / 100) * 100 * 0.75)
+
+        else:
+            wav += percentage_to_db((volume / 127) * 100 * 0.75)
+
         wav.export(wav_output_filename, format="wav")
 
 
